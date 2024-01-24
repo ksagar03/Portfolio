bc14e5f54b2ce4520a625dbe48a3456d9ae5cc15
ksagar03 <sagar22krishna03@gmail.com>
2024-01-24 21:33:19 +0530

    Updates: Added project thumbnail images

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,23 +6,28 @@ import Link from 'next/link'
 import Image from 'next/image'
 import GitHub from '@mui/icons-material/GitHub'
 import gamer_freak from "@/../../Portfolio/public/images/projects/gamerFreak.png"
+import portfolio from "@/../../Portfolio/public/images/projects/portfolio.png"
+import Todolist from "@/../../Portfolio/public/images/projects/Todolist.png"
+
+import {motion} from "framer-motion"
+const FramerImage =motion(Image)
 const Projects = () => {
   const FeaturedProjects = ({type,title, summary, img,link, githublink }) => {
 
     return(
       <article className='w-full flex items-center justify-between rounded-3xl border border-dark border-solid bg-light shadow-2xl p-12 relative rounded-br-2xl'>
          <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
-        <Link href={link}target="_blank" className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
-          <Image src={img} alt={title} className=' w-full h-auto'>
-            </Image>  
-        </Link>
+        <div className='w-1/2 overflow-hidden rounded-lg'>
+          <FramerImage src={img} alt={title} className=' w-full h-auto' 
+          whileHover={{scale: 1.05}}
+          transition={{duration: 0.5}}
+          />
+        </div>
         <div className='w-1/2 flex flex-col items-start justify-between pl-6 '>
           <span className=' text-blue-500 font-medium text-xl'>
             {type}
           </span>
-          <Link href={link} target='_blank' className=' hover:underline underline-offset-4'>
-            <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
-          </Link>
+            <h2 className='underline underline-offset-[5px] my-2 w-full text-left text-4xl font-bold'>{title}</h2> 
           <p className='my-2 font-medium text-dark '>
             {summary}
           </p>
@@ -43,17 +48,21 @@ const Projects = () => {
   const Project =({type, title ,img, link, githublink}) =>{
     return(
       <article className=' w-full flex flex-col items-center justify-center rounded-2xl border border-solid  border-dark bg-light p-6 relative shadow-2xl'>
-         <Link href={link}target="_blank" className='w-full cursor-pointer overflow-hidden rounded-lg'>
-          <Image src={img} alt={title} className=' w-full h-auto'>
-            </Image>  
-        </Link>
+         <div className="absolute top-0 -right-3 -z-10 w-[102.5%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+        
+          <div className='overflow-hidden rounded-lg w-full'>
+          <FramerImage src={img} alt={title} className=' w-full h-auto'
+          whileHover={{scale: 1.05}}
+          transition={{duration:0.5}}
+          
+          />    
+          </div>
         <div className='w-full flex flex-col items-start justify-between mt-4 '>
           <span className=' text-blue-500 font-medium text-xl'>
             {type}
           </span>
-          <Link href={link} target='_blank' className=' hover:underline underline-offset-4'>
-            <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
-          </Link>
+          
+            <h2 className='my-2 w-full text-left text-3xl font-bold underline underline-offset-[5px]'>{title}</h2>
           <div className=' mt-2 w-full flex items-center justify-between '>
           <Link href={link} target='_blank' className=' rounded-xl bg-dark text-light p-1 px-4 text-lg font-semibold hover:bg-light hover:text-dark border-2 border-bold border-transparent hover:border-dark hover:duration-500  '>
             Visit
@@ -83,7 +92,7 @@ const Projects = () => {
         className=' mb-12'
         
         />
-        <div className='grid grid-cols-12 gap-24'>
+        <div className='grid grid-cols-12 gap-24 gap-y-25'>
           <div className='col-span-12'>
             {/* Featured Project */}
             <FeaturedProjects 
@@ -98,8 +107,8 @@ const Projects = () => {
           </div>
           <div className='col-span-6'>
           <Project 
-            title="Todo list"
-            img={gamer_freak}
+            title="Todo-List"
+            img={Todolist}
             link="https://gamer-freak.web.app"
             type=" Crud Operation NextJS Web Application"
             githublink="https://github.com/ksagar03/gamer-freak"
@@ -110,7 +119,7 @@ const Projects = () => {
           <div className='col-span-6'>
           <Project 
             title="Portfolio"
-            img={gamer_freak}
+            img={portfolio}
             link="https://gamer-freak.web.app"
             type=" Crud Operation NextJS Web Application"
             githublink="https://github.com/ksagar03/gamer-freak"
